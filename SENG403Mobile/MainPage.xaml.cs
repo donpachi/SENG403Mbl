a38ec54f0b264e72f11b56b746afc2629de358d9
Kudoes <kashifmkhan95@hotmail.com>
2017-03-29 02:06:26 -0600

    Alarm works
    - Added the ability for a user to set an alarm, and dismiss/snooze an
    alarm for 1 minute

--- SENG403Mobile/MainPage.xaml.cs
+++ SENG403Mobile/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,15 +24,87 @@ namespace SENG403Mobile
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        AlarmHandler alarmHandler;
         public MainPage()
         {
+            
             this.InitializeComponent();
+            BackgroundMediaPlayer.Current.SetUriSource(new Uri("ms-winsoundevent:Notification.Looping.Alarm10"));
+            BackgroundMediaPlayer.Current.IsLoopingEnabled = true;
+            BackgroundMediaPlayer.Current.Pause();
             Clock.UpdateTimeEvent += UpdateTimeLabel;
+            //Dateblock.Text = DateTime.Now.DayOfWeek.ToString();//DateTime.Now.ToString();
+            alarmHandler = new AlarmHandler();
+            Alarm.onRing += OnAlarmRing;
+            // DateTime.Now.Date.ToString;
         }
 
         private void UpdateTimeLabel(object sender, String args)
         {
 
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BackgroundMediaPlayer.Current.SetUriSource(new Uri("ms-appx:///Sounds/sawarp1.wav"));
+
+                BackgroundMediaPlayer.Current.Play();
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    // handle exception.
+                    // For example: Log error or notify user problem with file
+                }
+            }
+        }
+
+        private void OnAlarmRing()
+        {
+            BackgroundMediaPlayer.Current.Play();
+            buttonDismissAlarm.Visibility = Visibility.Visible;
+            buttonSnoozeAlarm.Visibility = Visibility.Visible;
+            setAlarm.Visibility = Visibility.Collapsed;
+            timePicker.Visibility = Visibility.Collapsed;
+            confirmAlarm.Visibility = Visibility.Collapsed;
+        }
+
+        private void clickSnooze(object sender, RoutedEventArgs e)
+        {
+            buttonDismissAlarm.Visibility = Visibility.Collapsed;
+            buttonSnoozeAlarm.Visibility = Visibility.Collapsed;
+            BackgroundMediaPlayer.Current.Pause();
+            alarmHandler.currentAlarm.snooze(1);
+
+        }
+
+        private void clickDismiss(object sender, RoutedEventArgs e)
+        {
+            buttonDismissAlarm.Visibility = Visibility.Collapsed;
+            buttonSnoozeAlarm.Visibility = Visibility.Collapsed;
+            BackgroundMediaPlayer.Current.Pause();
+            alarmHandler.currentAlarm = null;
+        }
+
+        private void setClicked(object sender, RoutedEventArgs e)
+        {
+            setAlarm.Visibility = Visibility.Collapsed;
+            timePicker.Visibility = Visibility.Visible;
+            confirmAlarm.Visibility = Visibility.Visible;
+        }
+
+        private void confirmClicked(object sender, RoutedEventArgs e)
+        {
+            confirmAlarm.Visibility = Visibility.Collapsed;
+            timePicker.Visibility = Visibility.Collapsed;
+            setAlarm.Visibility = Visibility.Visible;
+
+            DateTime alarmTime = new DateTime();
+            alarmTime.Add(timePicker.Time);
+            alarmHandler.setNewAlarmTime(timePicker.Time.ToString());
         }
     }
 }
---
+++ SENG403Mobile/MissedAlarmHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENG403Mobile
+{
+    public class MissedAlarmHandler
+    {
+        static public event EventHandler MissedAlarm;
+
+        static public void triggerMissedAlarmEvent(object sender, EventArgs args)
+        {
+            if (MissedAlarm != null) MissedAlarm(sender, args);
+        }
+    }
+
+    public class MissedAlarmEventArgs : EventArgs
+    {
+
+    }
+}
